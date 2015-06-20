fedce11d59257bcb10d12c4e9ea1663ec5084f0a
cristianofer18 <cristiano.fer23@gmail.com>
2015-06-19 23:38:59 -0300

    add entity e controller produto

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private EfDbContext _context;
+
+        public IEnumerable<Produto> GetProdutos()
+        {             
+             return from produtos in _context.Produtos select produtos;
+        }
+
+    }
+}
