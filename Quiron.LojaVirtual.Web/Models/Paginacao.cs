188ae7699531c522f4277f7b745ae8fcf81b4b68
cristianofer18 <cristiano.fer23@gmail.com>
2015-06-25 19:48:44 -0300

    Classes de paginação

---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public Int32 ItensTotal { get; set; }
+        public Int32 ItensPorPagina { get; set; }
+        public Int32 PaginaAtual { get; set; }
+        public Int32 PaginaTotal { get { return (Int32)Math.Ceiling((decimal)ItensTotal/ItensPorPagina); } }
+    }
+}
