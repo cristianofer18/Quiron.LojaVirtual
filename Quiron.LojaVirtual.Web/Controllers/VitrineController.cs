e88999eec5bf4009b7c2dc596a189484aa3a5198
cristianofer18 <cristiano.fer23@gmail.com>
2015-06-26 20:59:55 -0300

    viewModel e paginacao

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -1,4 +1,5 @@
 ﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,26 @@ namespace Quiron.LojaVirtual.Web.Controllers
     public class VitrineController : Controller
     {
         private ProdutosRepositorio _repositorio;
-        private int ProdutosPorPagina = 3;
+        private int ProdutosPorPagina = 8;
         //
         // GET: /Vitrine/
-        public ActionResult ListaProdutos(int pagina = 1)
+        public ViewResult ListaProdutos(int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.GetProdutos.OrderBy(p => p.Descricao)
-                .Skip((pagina - 1) * ProdutosPorPagina).Take(ProdutosPorPagina);
 
-            return View(produtos);
+            var viewModel = new ProdutosViewModel
+            {
+                Produtos = _repositorio.GetProdutos.OrderBy(p => p.Descricao)
+                .Skip((pagina - 1) * ProdutosPorPagina).Take(ProdutosPorPagina),
+                Paginacao = new Paginacao
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = ProdutosPorPagina,
+                    ItensTotal = _repositorio.GetProdutos.Count()
+                }
+            };
+
+            return View(viewModel);
         }
-	}
+    }
 }
